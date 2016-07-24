7ed842f71c69c899814ae26000df92d06f940b83
Chase Wang <chase@ocwang.com>
2016-07-23 18:42:14 -0700

    Working version of public chatroom

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,11 +2,11 @@ defmodule MessengerClone.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", MessengerClone.RoomChannel
+  channel "rooms:*", MessengerClone.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
